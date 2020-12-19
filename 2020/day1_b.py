6d9a85abe144c8682c1a80e5a7fc95ca99638751
Brian Kimmel <kimmelb@gmail.com>
2020-12-18 21:27:26 -0500

    Day 1b completed

--- 2020/day1_b.py
+++ 2020/day1_b.py
@@ -1,32 +1,12 @@
-def check(original_array, current_parents, count_target, sum_target):
-    print(current_parents)
-    if len(current_parents)+1 == count_target:
-        for val in original_array :
-            val = int(val)
-            if(val not in current_parents):
-                sm = 0
-                product = 1
-                for parent in current_parents:
-                    sm += parent
-                    product *= parent
-                sm += val
-                if(sm == sum_target):
-                    return product * val
-                else:
-                    return 0
-    else:
-        for val in original_array:
-            if(val not in current_parents):
-                new_parents = current_parents.copy()
-                new_parents = new_parents.append(val)
-                print(new_parents)
-                retval = check(original_array, new_parents, count_target, sum_target)
-                if(retval) != 0:
-                    return retval
+
 
 
 contents = open("day1.input","r").read()
 content_array = contents.splitlines()
 
-prod = check(content_array, [1], 3, 2020)
-print(prod)
+for a in content_array:
+    for b in content_array:
+        for c in content_array:
+            if(int(a) + int(b) + int(c) == 2020):
+                print (f"{int(a) * int(b) * int(c)}")
+                exit()
