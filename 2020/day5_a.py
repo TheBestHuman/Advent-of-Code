7f3b85ca017bf8b83bff4f1587781daba6f01e21
Brian Kimmel <kimmelb@gmail.com>
2020-12-22 20:23:55 -0500

    day 5 complete

---
+++ 2020/day5_a.py
@@ -0,0 +1,40 @@
+import math
+
+def incrementor(min, max, content_char, towards_zero_char):
+    if(content[i] == towards_zero_char):
+        max = math.floor((max-min)/2) + min
+    else:
+        min = math.ceil((max-min)/2) + min 
+    return min, max
+
+contents = open("day5.input","r").read()
+
+content_array = contents.split("\n")
+
+max = 127
+min = 0
+
+max_seat_id = 0
+for content in content_array:
+    max = 127
+    min = 0
+
+    for i in range(0,7):
+        min, max = incrementor(min, max, content[i], 'F')
+
+    row = max
+
+    min = 0
+    max = 7
+    for i in range(7,10):
+        min, max = incrementor(min, max, content[i], 'L')
+    
+    seat = max
+
+    seat_id = (row * 8) + seat
+    if seat_id > max_seat_id:
+        max_seat_id = seat_id
+
+print(max_seat_id)
+
+
